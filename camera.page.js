339fabb282f04ea5266441b07af08ea1d56b3662
Anubhav <anubhav15129@iiitd.ac.in>
2020-03-16 21:56:14 +0530

    MVP

--- camera.page.js
+++ camera.page.js
@@ -7,6 +7,7 @@ import styles from './style';
 import Toolbar from './toolbar.component';
 import Gallery from './gallery.component';
 import config from './config';
+import config2 from './config2';
 // var RNFS = require('react-native-fs');
 // import * as RNFS from 'react-native-fs';
 
@@ -20,6 +21,7 @@ import config from './config';
 export default class CameraPage extends React.Component {
     camera = null;
 
+
     state = {
         captures: [],
         // setting flash to be turned off by default
@@ -32,7 +34,11 @@ export default class CameraPage extends React.Component {
         visionResponse: '',
         loading: false,
         googleVisionDetetion: undefined
+
     };
+    // constructor(){
+    //         this.myRef = React.createRef();
+    // };
 
     setFlashMode = (flashMode) => this.setState({ flashMode });
     setCameraType = (cameraType) => this.setState({ cameraType });
@@ -43,71 +49,86 @@ export default class CameraPage extends React.Component {
             this.camera.stopRecording();
     };
 
+    // getTranslation(term, target, key){
+    //   fetch("https://translation.googleapis.com/language/translate/v2", {
+    //         method: "POST",
+    //         headers: {
+    //           "Content-Type": "application/json; charset=utf-8",
+    //           Authorization: `Bearer ${key}`
+    //         },
+    //         redirect: "follow",
+    //         referrer: "no-referrer",
+    //         body: JSON.stringify({
+    //           q: term,
+    //           target: target
+    //         })
+    //       }).then(googleTranslateRes => {
+    //             // console.log(googleVisionRes)
+    //             console.log(googleTranslateRes)
+
+    //             if (googleTranslateRes) {
+    //                 this.setState(
+    //                     {
+    //                         loading: false,
+    //                         // googleVisionDetetion: googleVisionRes.responses[0].localizedObjectAnnotations[0].name,
+    //                     }
+    //                 )
+    //                return googleTranslateRes;
+    //             }
+    //         }).catch((error) => { console.log(error) })
+              
+    // };
+
+    translate = async (term, target, key) => {
+
+        // console.log("hereee", string1);
+
+        // let googleTranslate2 = await fetch(config.googleCloud.api + config.googleCloud.apiKey, {
+        //     method: "POST",
+        //     headers: {
+        //       "Content-Type": "application/json; charset=utf-8",
+        //     },
+        //     redirect: "follow",
+        //     referrer: "no-referrer",
+        //     body: JSON.stringify({
+        //       q: term,
+        //       target: target
+        //     })
+        //   });
+        var url = "https://translation.googleapis.com/language/translate/v2";
+        //Strings requiring translation
+        url += "?q=" + term;
+
+        // url += "&q=" + escape($("#title").text());
+        //Target language
+        
+        url += "&target=" + "de";
+        //Replace with your API key
+        
+        url += "&key=AIzaSyB6YoT679YdcMPII-M1hDq0oM6RGLYOQzI";
+        let googleTranslate = await fetch(url);
+
+        await googleTranslate.json()
+            .then(googleTranslateRes => {
+                // console.log(googleTranslateRes)
+                console.log(googleTranslateRes.data.translations[0].translatedText);
+
+
+                if (googleTranslateRes) {
+                    this.setState(
+                        {
+                            loading: false,
+                            // googleVisionDetetion: googleVisionRes.responses[0].localizedObjectAnnotations[0].name,
+                        }
+                    )
+                   return googleTranslateRes;
+                }
+            }).catch((error) => { console.log(error) })
+
+    }
+
     handlePress = async (base64) => {
-        // fetch('http://192.168.43.249:3000/',{
-        //     method: 'POST',
-        //     image: photoData,
-        //     headers: {"Content-Type": "img"}
-        // })
-        // .then(function(response){
-        //     // console.log(response);
-        //     return response.json()
-        // }).catch(error => console.log(error));
 
-
-
-        // var data = new FormData();
-        // data.append('fileData', { uri: photoData.uri, name: 'picture.jpg', type: 'image/jpg' });
-        // // Create the config object for the POST
-        //  const config = {
-        //   method: 'POST',
-        //   headers: {
-        //    'Accept': 'application/json',
-        //    'Content-Type': 'multipart/form-data',
-        //   },
-        //   body: data,
-        //  };
-        // fetch('http://192.168.43.249:3000/', config).then(responseData => {
-        //     // Log the response form the server // Here we get what we sent to Postman back
-        //     console.log(responseData);
-        // })
-        // .catch(err => { console.log(err); });
-        // };
-
-
-
-        // var image = photoData.uri; 
-
-        // const Blob = RNFetchBlob.polyfill.Blob;
-        // const fs = RNFetchBlob.fs;
-        // window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
-        // window.Blob = Blob;
-
-       
-        // let uploadBlob = null
-        // const imageRef = Firebase.storage().ref('posts').child("test.jpg")
-        // let mime = 'image/jpg'
-        // fs.readFile(image, 'base64')
-        //   .then((data) => {
-        //     return Blob.build(data, { type: `${mime};BASE64` })
-        // })
-        // .then((blob) => {
-        //     uploadBlob = blob
-        //     return imageRef.put(blob, { contentType: mime })
-        //   })
-        //   .then(() => {
-        //     uploadBlob.close()
-        //     return imageRef.getDownloadURL()
-        //   })
-        //   .then((url) => {
-        //     // URL of the image uploaded on Firebase storage
-        //     console.log(url);
-            
-        //   })
-        //   .catch((error) => {
-        //     console.log(error);
-
-        //   }) 
 
         let googleVisionRes = await fetch(config.googleCloud.api + config.googleCloud.apiKey, {
             method: 'POST',
@@ -130,15 +151,32 @@ export default class CameraPage extends React.Component {
 
         await googleVisionRes.json()
             .then(googleVisionRes => {
-                console.log(googleVisionRes)
+                // console.log(googleVisionRes)
+                console.log(googleVisionRes.responses[0].localizedObjectAnnotations[0].name)
+                // this.translate(googleVisionRes.responses[0].localizedObjectAnnotations[0].name);
+                this.translate(googleVisionRes.responses[0].localizedObjectAnnotations[0].name, "de", "AIzaSyB6YoT679YdcMPII-M1hDq0oM6RGLYOQzI")
                 if (googleVisionRes) {
                     this.setState(
                         {
                             loading: false,
-                            googleVisionDetetion: googleVisionRes.responses[0]
+                            googleVisionDetetion: googleVisionRes.responses[0].localizedObjectAnnotations[0].name,
+                            googleVisionDetetion_x1 : googleVisionRes.responses[0].localizedObjectAnnotations[0].boundingPoly.normalizedVertices[0].x,
+                            googleVisionDetetion_y1 : googleVisionRes.responses[0].localizedObjectAnnotations[0].boundingPoly.normalizedVertices[0].y,
+                            googleVisionDetetion_x2 : googleVisionRes.responses[0].localizedObjectAnnotations[0].boundingPoly.normalizedVertices[1].x,
+                            googleVisionDetetion_y2 : googleVisionRes.responses[0].localizedObjectAnnotations[0].boundingPoly.normalizedVertices[1].y,
+                            googleVisionDetetion_x3 : googleVisionRes.responses[0].localizedObjectAnnotations[0].boundingPoly.normalizedVertices[2].x,
+                            googleVisionDetetion_y3 : googleVisionRes.responses[0].localizedObjectAnnotations[0].boundingPoly.normalizedVertices[2].y,
+                            googleVisionDetetion_x4 : googleVisionRes.responses[0].localizedObjectAnnotations[0].boundingPoly.normalizedVertices[3].x,
+                            googleVisionDetetion_y4 : googleVisionRes.responses[0].localizedObjectAnnotations[0].boundingPoly.normalizedVertices[3].y,
+                            
                         }
                     )
-                    console.log('this.is response', this.state.googleVisionDetetion);
+                    // this.drawBox(googleVisionRes.responses[0].localizedObjectAnnotations[0], googleVisionRes.responses[0].localizedObjectAnnotations[0].name)
+                    // var elements = document.getElementsByClassName('Output');
+                    // var elements = this.myRef.current;
+                    // elements.innerHTML = googleVisionRes.responses[0].localizedObjectAnnotations[0].name;                    
+
+                    return googleVisionRes.responses[0].localizedObjectAnnotations[0];
                 }
             }).catch((error) => { console.log(error) })
     
@@ -161,7 +199,8 @@ export default class CameraPage extends React.Component {
         const options = { quality: 0.5, base64: true};
             
         const photoData = await this.camera.takePictureAsync(options);
-        console.log(photoData);
+        // console.log(photoData);
+
         // photoData = RNFS.readFile(photoData);
         // console.log(photoData.uri);
         this.setState({
@@ -169,7 +208,13 @@ export default class CameraPage extends React.Component {
                 result: photoData.base64,
                 camera: false
             }, () =>
-                    this.handlePress(this.state.result))
+                    this.handlePress(this.state.result).then(googleVisionRes => { 
+                        if(googleVisionRes){
+                            console.log('OUTPUT', googleVisionRes);
+                            this.drawBox(googleVisionRes, googleVisionRes.name)
+                        }
+                        
+                    }))
         // this.handlePress(photoData.base64);
         this.setState({ capturing: false, captures: [photoData, ...this.state.captures] });
     };
@@ -187,6 +232,49 @@ export default class CameraPage extends React.Component {
         // console.log(hasCameraPermission)
         this.setState({hasCameraPermission: hasCameraPermission});
     };
+    // drawing the boxes aronund the detected text elements
+    drawRectangles() {
+        const {textElements} = this.state;
+        return textElements.map((element, index) => this.drawBox(element, index));
+    };
+
+
+    drawBox(e, i){
+        let [left, top, right, bottom] = e.boundingBox;
+        // const {imageWidh, imageHeight, imgPixelWidth, imgPixelHeight} = this.state;
+
+        // calculating the absolute position of each box base on the pixel size of the 
+        // of the image and the on screen sizes (imageWidh, imageHeight) given by react native 
+        // (check the renderCameraOrImage funtion) 
+                           // <Text visible="false" value="heeey" className="Output" ref={this.myRef} />
+                
+
+        // left = (left / imgPixelWidth) * imageWidh;
+        // right = (right / imgPixelWidth) * imageWidh;
+        // top = (top / imgPixelHeight) * imageHeight;
+        // bottom = (bottom / imgPixelHeight) * imageHeight;
+
+        const boxWidth = Math.abs(left - right);
+        const boxHeight = Math.abs(top - bottom);
+
+        console.log('box', boxWidth, boxHeight);
+
+        return (
+          <TouchableOpacity
+            key={i}
+            style={{
+              position: 'absolute',
+              left,
+              top,
+              width: boxWidth,
+              height: boxHeight,
+              borderWidth: 1,
+              borderColor: Colors.green,
+              zIndex: 2000,
+            }}
+          />
+        );
+    };
 
     render() {
         const { hasCameraPermission, flashMode, cameraType, capturing, captures, googleVisionDetetion, result, loading} = this.state;
@@ -199,15 +287,32 @@ export default class CameraPage extends React.Component {
         return (
             <React.Fragment>
                 <View>
+                    
                     <Camera
                         type={cameraType}
                         flashMode={flashMode}
                         style={styles.preview}
+                        androidCameraPermissionOptions={{
+                          title: 'Permission to use camera',
+                          message: 'We need your permission to use your camera',
+                          buttonPositive: 'Ok',
+                          buttonNegative: 'Cancel',
+                        }}
                         ref={camera => this.camera = camera}
                     />
                 </View>
 
-                {captures.length > 0 && <Gallery captures={captures}/>}
+                <View style={styles.reactangleContainer}>
+                     <View style={styles.rectangle}>
+                          <View style={styles.rectangleColor} />
+                          <View style={styles.topLeft} />
+                          <View style={styles.topRight} />
+                          <View style={styles.bottomLeft} />
+                          <View style={styles.bottomRight} />
+                     </View>
+                     <Text> Helloooo </Text>
+                     
+                </View>
 
                 <Toolbar 
                     capturing={capturing}
@@ -224,3 +329,5 @@ export default class CameraPage extends React.Component {
         );
     };
 };
+
+
--- style.js
+++ style.js
@@ -3,6 +3,11 @@ import { StyleSheet, Dimensions } from 'react-native';
 
 const { width: winWidth, height: winHeight } = Dimensions.get('window');
 
+const deviceHeight = Dimensions.get("window").height;
+
+const deviceWidth = Dimensions.get("window").width;
+
+
 export default StyleSheet.create({
     preview: {
         height: winHeight,
@@ -54,6 +59,74 @@ export default StyleSheet.create({
     galleryImage: { 
         width: 75, 
         height: 75 
-    }
+    },
+
+	rectangleContainer: {
+       	 flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'transparent',
+  	},
+  rectangle: {
+      borderLeftColor: 'rgba(0, 0, 0, .6)',
+      borderRightColor: 'rgba(0, 0, 0, .6)',
+      borderTopColor: 'rgba(0, 0, 0, .6)',
+      borderBottomColor: 'rgba(0, 0, 0, .6)',
+      borderLeftWidth: deviceWidth / 10,
+      borderRightWidth: deviceWidth / 10,
+      borderTopWidth: deviceHeight / 6,
+      borderBottomWidth: deviceHeight / 4
+  },
+  rectangleColor: {
+      height: 450,
+      width: 500,
+      backgroundColor: 'transparent'
+  },
+  topLeft: {
+      width: 50,
+      height: 50,
+      borderTopWidth: 2,
+      borderLeftWidth: 2,
+      position: 'absolute',
+      left: -1,
+      top: -1,
+      borderLeftColor: 'white',
+      borderTopColor: 'white'
+  },
+  topRight: {
+      width: 50,
+      height: 50,
+      borderTopWidth: 2,
+      borderRightWidth: 2,
+      position: 'absolute',
+      right: -1,
+      top: -1,
+      borderRightColor: 'white',
+      borderTopColor: 'white'
+  },
+  bottomLeft: {
+      width: 50,
+      height: 50,
+      borderBottomWidth: 2,
+      borderLeftWidth: 2,
+      position: 'absolute',
+      left: -1,
+      bottom: -1,
+      borderLeftColor: 'white',
+      borderBottomColor: 'white'
+  },
+  bottomRight: {
+      width: 50,
+      height: 50,
+      borderBottomWidth: 2,
+      borderRightWidth: 2,
+      position: 'absolute',
+      right: -1,
+      bottom: -1,
+      borderRightColor: 'white',
+      borderBottomColor: 'white'
+  }
+
+
 });
 
